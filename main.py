fc6131d9b0a850f06ea993db47181d27f6f619bd
kaz101 <kaz101130@gmail.com>
2020-05-13 21:16:04 -0400

    add event handler

--- main.py
+++ main.py
@@ -1,53 +1,71 @@
 #! /usr/bin/env python3
 
+# Import the nessesary modules
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication
 import sys
 import spiral
 from functions import *
 import threading
-
+import time
+# Maim Window class
 class Spiral(QtWidgets.QMainWindow, spiral.Ui_MainWindow):
     def __init__(self, parent=None):
         super(Spiral, self).__init__(parent)
+
+        # Set up the UI
+        self.setWindowFlags(QtCore.Qt.Window | QtCore.Qt.WindowStaysOnTopHint| QtCore.Qt.FramelessWindowHint)
         self.setupUi(self)
+        self.setProperty('windowOpacity',0.1)
+        self.screen_width = QtWidgets.QDesktopWidget().screenGeometry().width()
+        self.move(self.screen_width/2 - self.width()/2, 0)
+
+        # Set up chrome browser
+        self.chrome_options = webdriver.ChromeOptions()
+        self.chrome_options.add_argument('--incognito')
+        self.chrome_options.add_argument('--start-maximized')
+        self.webpage = webdriver.Chrome(chrome_options=self.chrome_options)
+
+        # Declare Variables
         self.all_interests = {}
         self.all_links = []
         self.interests = []
+
+        # Get the list of interets from interests.txt file
         self.get_interests()
-    #self.webview.setUrl(QtCore.QUrl('https://google.com'))
+
+        # Add 'all interests' to QComboBox at position 0
+        self.interests_combo_box.addItem('All Interests')
+
+        # Connect the button events
         self.next_button.clicked.connect(lambda: self.next_page())
-        self.interests_list_box.addItem('All Interests')
-        self.interests_list_box.setCurrentRow(0)
         self.add_button.clicked.connect(lambda: self.add_interest())
         self.remove_button.clicked.connect(lambda: self.remove_interest())
+
+        # Start a new thread for loading interests
         self.t1 = threading.Thread(target=lambda: self.background_load())
-        self.webpage = webdriver.Chrome()
         self.t1.start()
 
+    # Function to load all interests on start
     def background_load(self):
         threads = []
         for i in self.interests:
             t = threading.Thread(target=lambda: self.load_interest(i))
             t.start()
 
+    # Function to load the next page
     def next_page(self):
-        interest = self.interests_list_box.currentRow()
-        if interest == 0:
+        interest = self.interests_combo_box.currentIndex() # Get the chosen interest
+        if interest == 0:                                  # index 0 is always 'all interests'
             page = load_page(self.all_links)
         else:
-            print (self.interests_list_box.currentItem().text())
-            page = load_page(self.all_interests[self.interests_list_box.currentItem().text()])
+            page = load_page(self.all_interests[self.interests_combo_box.itemText(self.interests_combo_box.currentIndex())])
         try:
-            print('webpage')
             self.webpage.get(page)
-
-        #    self.webview.setUrl(QtCore.QUrl(page))
         except:
-            next_page()
+            self.next_page()
 
-
-
+    # Function to add a new interest from the add item box
     def add_interest(self):
         if self.add_item_box.text() and self.add_item_box.text() not in self.interests:
             t = threading.Thread(target=lambda: self.load_interest(self.add_item_box.text()))
@@ -55,13 +73,15 @@ class Spiral(QtWidgets.QMainWindow, spiral.Ui_MainWindow):
             self.interests.append(self.add_item_box.text())
             self.save_interests()
 
+    # Function to remove and interest from everywhere
     def remove_interest(self):
         try:
-            del self.all_interests[self.interests_list_box.currentItem().text()]
+            del self.all_interests[self.interests_combo_box.currentText()]
         except KeyError:
+                print('key error')
                 return
-        self.interests.pop(self.interests_list_box.currentRow()-1)
-        self.interests_list_box.takeItem(self.interests_list_box.currentRow())
+        self.interests.remove(self.interests_combo_box.currentText())
+        self.interests_combo_box.removeItem(self.interests_combo_box.currentIndex())
         self.all_links.clear()
         for key, value in self.all_interests.items():
             self.all_links.extend(value)
@@ -74,7 +94,7 @@ class Spiral(QtWidgets.QMainWindow, spiral.Ui_MainWindow):
         driver, list = search_google_images(interest,driver)
         self.all_interests[interest] = list
         self.all_links.extend(list)
-        self.interests_list_box.addItem(interest)
+        self.interests_combo_box.addItem(interest)
         self.add_item_box.clear()
         driver.close()
 
@@ -92,9 +112,27 @@ class Spiral(QtWidgets.QMainWindow, spiral.Ui_MainWindow):
             self.interests[i] = self.interests[i].rstrip()
         save_file.close()
 
+    def eventFilter(self,obj, event):
+        if event.type() == QtCore.QEvent.HoverEnter:
+            self.setProperty('windowOpacity',1)
+            return True
+
+        if event.type() == QtCore.QEvent.HoverLeave:
+            self.setProperty('windowOpacity', 0.1)
+            return True
+        if event.type() == QtCore.QEvent.KeyPress:
+            self.add_interest()
+            print('key')
+            return True
+
+
+        else:
+            return False
+
 def main():
     app = QApplication(sys.argv)
     form = Spiral()
+    form.installEventFilter(form)
     form.show()
     app.exec_()
 if __name__ == '__main__':
